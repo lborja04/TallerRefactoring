735d52548bd684258028fbc55dedd3f01003e3dd
lborja04 <139704941+lborja04@users.noreply.github.com>
2024-01-05 00:02:29 -0500

    Create Paper.java

---
+++ Paper.java
@@ -0,0 +1,11 @@
+public class Paper extends Choice{
+  
+  public Paper(ArrayList<Choice> winAgainst){
+    super(ArrayList<Choice> winAgainst);
+  }
+
+  public int checkAgainst(Choice choice){
+    //lógica para revisar que jugada gana, devuelve 1 si gana this, 0 si es igual, -1 si pierde el this
+  }
+  
+}
