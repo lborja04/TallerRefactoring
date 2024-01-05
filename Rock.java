1b4fa5aa11caf3a453b8aafba8cc0cc83ea3a784
lborja04 <139704941+lborja04@users.noreply.github.com>
2024-01-05 00:02:08 -0500

    Create Rock.java

---
+++ Rock.java
@@ -0,0 +1,11 @@
+public class Rock extends Choice{
+  
+  public Rock(ArrayList<Choice> winAgainst){
+    super(ArrayList<Choice> winAgainst);
+  }
+
+  public int checkAgainst(Choice choice){
+    //lógica para revisar que jugada gana, devuelve 1 si gana this, 0 si es igual, -1 si pierde el this
+  }
+  
+}
