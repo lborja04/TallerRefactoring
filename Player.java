342f73f608d28143962cf9b3c757bf962051c569
lborja04 <139704941+lborja04@users.noreply.github.com>
2024-01-05 00:01:23 -0500

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,16 @@
+public class Player{
+  private int wins;
+  
+  public Choice playerChoice(){
+    //lógica para elegir un Choice al azar
+  }
+
+  public void setWins(int wins){
+    this.wins=wins;
+  }
+
+  public int getWins(){
+    return this.wins;
+  }
+  
+}
